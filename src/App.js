8d51ad28ef23be7e427a8897c4598350f1abaf60
Gregory Marklew <greg_marklew@hotmail.com>
2021-10-07 18:16:46 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import foodsJSON from "./foods.json";
+import React, { useState } from "react";
 function App() {
+  const [foods, setFoods] = useState(foodsJSON)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {foods.map((food) => {
+        return(
+        <div>
+        <p>{food.name}</p>
+        <img src={food.image} width="100px" alt={food.name} />
+        </div>
+        );
+        })};
     </div>
   );
 }
-
 export default App;
